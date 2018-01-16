bb3cfc181a7954323404d5bb40f2e78b1f006ac5
Ai Nguyen <ainguyenkaka@gmail.com>
2018-01-16 10:12:44 +0800

    add blank for tutorial link

--- src/main/webapp/app/page/tutorial/tutorial.controller.js
+++ src/main/webapp/app/page/tutorial/tutorial.controller.js
@@ -5,16 +5,17 @@
         .module('webApp')
         .controller('TutorialController', TutorialController);
 
-    TutorialController.$inject = ['$scope', '$state', 'TutorialService','DataUtils'];
+    TutorialController.$inject = ['$scope', '$state', 'TutorialService', 'DataUtils'];
 
-    function TutorialController($scope, $state, TutorialService,DataUtils) {
+    function TutorialController($scope, $state, TutorialService, DataUtils) {
         var vm = this;
         vm.tutorials = [];
+        vm.promise = {};
 
         loadTutorials();
 
         function loadTutorials() {
-            TutorialService.getTutorials({}, onSuccess, onError);
+            vm.promise = TutorialService.getTutorials({}, onSuccess, onError);
 
             function onSuccess(data) {
                 vm.tutorials = DataUtils.getArrayDataFromSheet(data);
